aa4eba3e3f5a4ba8207f91f315145e5ac6182e26
jayden <mug4ct@virginia.edu>
2025-02-26 01:22:47 -0500

    fixed bug where redirect method in library/views.py was overriding django's redirect method by renaming it

--- library/views.py
+++ library/views.py
@@ -20,9 +20,9 @@ def explore_library(request):
         book = Book.objects.get(id=book_id)
         if request.user.is_authenticated:
             user_library.books.add(book)
-            return redirect('library/my_library')
+            return redirect('my_library')
         else:
-            return redirect('library/login')
+            return redirect('login')
     
     return render(request, 'library/explore_library.html', {
         'books': books,
@@ -35,8 +35,9 @@ def my_library(request):
     if request.method == 'POST':
         book_id = request.POST.get('book_id')
         book = Book.objects.get(id=book_id)
-        user_library.books.remove(book)
-        return render(request, 'library/my_library.html')
+        if request.user.is_authenticated:
+            user_library.books.remove(book)
+            return redirect('my_library')
     return render(request, 'library/my_library.html', {'books': user_library.books.all()})
 
 def custom_logout(request):
@@ -50,31 +51,31 @@ def register(request):
             user = form.save()
             # Set the default role to 'patron'
             Profile.objects.create(user=user, role='patron')
-            login(request, user)
-            return render(request, 'library/home.html')
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('home')
     else:
         form = CustomUserCreationForm()
     return render(request, 'library/register.html', {'form': form})
 
 @login_required
-def redirect(request):
+def assign_role(request):
     librarian_emails = ["bjayden36@gmail.com", "chankyu2004@gmail.com", "haolinchen203@gmail.com", "xsn5hw@virginia.edu"]
     profile, created = Profile.objects.get_or_create(user=request.user)
     if request.user.email in librarian_emails:
         profile.role = 'librarian'
         profile.save()
-        return render(request, 'library/librarian_dashboard.html')
+        return redirect('librarian_dashboard')
     else: 
         profile.role = 'patron'
         profile.save()
 
-    return render(request, 'library/my_library.html')
+    return redirect('my_library')
 
 
 @login_required
 def librarian_dashboard(request):
     if request.user.profile.role != 'librarian':
-        return render(request, 'library/home.html')
+        return redirect('home')
     
     books = Book.objects.all()
 
@@ -83,6 +84,6 @@ def librarian_dashboard(request):
         author = request.POST.get('author')
         if title and author:
             Book.objects.create(title=title, author=author)
-            return redirect('librarian')
+            return redirect('librarian_dashboard')
 
     return render(request, 'library/librarian_dashboard.html', {'books': books})
