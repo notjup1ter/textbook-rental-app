b611519ec1f09933935139530905a8414d1f9f8d
Nemo0men <chankyu2004@gmail.com>
2025-03-15 23:20:00 -0400

    allow users to change password

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
@@ -12,4 +13,15 @@ urlpatterns = [
     path('logout/', views.custom_logout, name='logout'),
     path('redirect/', views.assign_role, name='redirect'),
     path('profile/', views.profile, name='profile'),
+    path('change-password/', 
+        login_required(auth_views.PasswordChangeView.as_view(
+            template_name='library/change_password.html',
+            success_url='/library/change-password-done/'
+        )), 
+        name='change_password'),
+    path('change-password-done/', 
+        login_required(auth_views.PasswordChangeDoneView.as_view(
+            template_name='library/change_password_done.html'
+        )),
+        name='password_change_done'),
 ] 
