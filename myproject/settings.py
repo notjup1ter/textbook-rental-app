2eb6b413cdae60622ff254bf2c69b53385730b12
Nemo0men <chankyu2004@gmail.com>
2025-03-15 21:54:12 -0400

    aesthetics work now

--- myproject/settings.py
+++ myproject/settings.py
@@ -154,10 +154,7 @@ USE_TZ = True
 
 # SHERRIFF: Added the static_root variable here to fix an erorr with static files not being found
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'library/static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
